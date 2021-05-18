00b9ba76ddda2ed9678b1723abfbf9f9a0823ba6
Sandeep Mistry <sandeep.mistry@arm.com>
2021-05-18 18:44:13 -0400

    Add more comments to examples

--- examples/hello_pdm_microphone/main.c
+++ examples/hello_pdm_microphone/main.c
@@ -3,6 +3,9 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  * 
+ * This examples captures data from a PDM microphone using a sample
+ * rate of 8 kHz and prints the sample values over the USB serial
+ * connection.
  */
 
 #include <stdio.h>
@@ -12,21 +15,36 @@
 #include "pico/pdm_microphone.h"
 #include "tusb.h"
 
-struct pdm_microphone_config config = {
+// configuration
+const struct pdm_microphone_config config = {
+    // GPIO pin for the PDM CLK signal
     .gpio_clk = 2,
+
+    // GPIO pin for the PDM DAT signal
     .gpio_data = 3,
+
+    // PIO instance to use
     .pio = pio0,
+
+    // PIO State Machine instance to use
     .pio_sm = 0,
+
+    // sample rate in Hz
     .sample_rate = 8000,
+
+    // number of samples to buffer
     .sample_buffer_size = 256,
 };
 
+// variables
 int16_t sample_buffer[256];
 volatile int samples_read = 0;
 
 void on_pdm_samples_ready()
 {
-  samples_read = pdm_microphone_read(sample_buffer, 256);
+    // callback from library when all the samples in the library
+    // internal sample buffer are ready for reading 
+    samples_read = pdm_microphone_read(sample_buffer, 256);
 }
 
 int main( void )
@@ -39,26 +57,28 @@ int main( void )
 
     printf("hello PDM microphone\n");
 
+    // initialize the PDM microphone
     if (pdm_microphone_init(&config) < 0) {
         printf("PDM microphone initialization failed!\n");
-        while (1) {
-            tight_loop_contents();
-        }
+        while (1) { tight_loop_contents(); }
     }
 
+    // set callback that is called when all the samples in the library
+    // internal sample buffer are ready for reading
     pdm_microphone_set_samples_ready_handler(on_pdm_samples_ready);
     
+     // start capturing data from the PDM microphone
     if (pdm_microphone_start() < 0) {
         printf("PDM microphone start failed!\n");
-        while (1) {
-            tight_loop_contents();
-        }
+        while (1) { tight_loop_contents(); }
     }
 
     while (1) {
+         // store and clear the samples read from the callback
         int sample_count = samples_read;
         samples_read = 0;
         
+        // loop through any new collected samples
         for (int i = 0; i < sample_count; i++) {
             printf("%d\n", sample_buffer[i]);
         }
--- examples/usb_microphone/main.c
+++ examples/usb_microphone/main.c
@@ -3,12 +3,20 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  * 
+ * This examples creates a USB Microphone device using the TinyUSB
+ * library and captures data from a PDM microphone using a sample
+ * rate of 16 kHz, to be sent the to PC.
+ * 
+ * The USB microphone code is based on the TinyUSB audio_test example.
+ * 
+ * https://github.com/hathach/tinyusb/tree/master/examples/device/audio_test
  */
 
 #include "pico/pdm_microphone.h"
 
 #include "usb_microphone.h"
 
+// configuration
 const struct pdm_microphone_config config = {
   .gpio_clk = 2,
   .gpio_data = 3,
@@ -18,21 +26,26 @@ const struct pdm_microphone_config config = {
   .sample_buffer_size = SAMPLE_BUFFER_SIZE,
 };
 
+// variables
 uint16_t sample_buffer[SAMPLE_BUFFER_SIZE];
 
+// callback functions
 void on_pdm_samples_ready();
 void on_usb_microphone_tx_ready();
 
 int main(void)
 {
+  // initialize and start the PDM microphone
   pdm_microphone_init(&config);
   pdm_microphone_set_samples_ready_handler(on_pdm_samples_ready);
   pdm_microphone_start();
 
+  // initialize the USB microphone interface
   usb_microphone_init();
   usb_microphone_set_tx_ready_handler(on_usb_microphone_tx_ready);
 
   while (1) {
+    // run the USB microphone task continuously
     usb_microphone_task();
   }
 
@@ -41,10 +54,18 @@ int main(void)
 
 void on_pdm_samples_ready()
 {
+  // Callback from library when all the samples in the library
+  // internal sample buffer are ready for reading.
+  //
+  // Read new samples into local buffer.
   pdm_microphone_read(sample_buffer, SAMPLE_BUFFER_SIZE);
 }
 
 void on_usb_microphone_tx_ready()
 {
+  // Callback from TinyUSB library when all data is ready
+  // to be transmitted.
+  //
+  // Write local buffer to the USB microphone
   usb_microphone_write(sample_buffer, sizeof(sample_buffer));
 }
