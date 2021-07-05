a6685f36549ecc995f0af1317ecc93869c872aa8
Sandeep Mistry <sandeep.mistry@arm.com>
2021-07-05 13:33:32 -0400

    Update usb_microphone example to be compatible with Pico SDK v1.2.0

--- examples/usb_microphone/usb_microphone.h
+++ examples/usb_microphone/usb_microphone.h
@@ -11,11 +11,11 @@
 #include "tusb.h"
 
 #ifndef SAMPLE_RATE
-#define SAMPLE_RATE (CFG_TUD_AUDIO_EPSIZE_IN / 2) * 1000
+#define SAMPLE_RATE (CFG_TUD_AUDIO_EP_SZ_IN / 2) * 1000
 #endif
 
 #ifndef SAMPLE_BUFFER_SIZE
-#define SAMPLE_BUFFER_SIZE (CFG_TUD_AUDIO_TX_FIFO_SIZE/2)
+#define SAMPLE_BUFFER_SIZE (CFG_TUD_AUDIO_EP_SZ_IN/2)
 #endif
 
 typedef void (*usb_microphone_tx_ready_handler_t)(void);
