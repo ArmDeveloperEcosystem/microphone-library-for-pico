6325e4e04e30a019fed35c7c1818cfa28329ed1f
Sandeep Mistry <sandeep.mistry@arm.com>
2021-09-23 14:48:42 -0400

    Add one extra sample to CFG_TUD_AUDIO_EP_SZ_IN
    As per https://github.com/hathach/tinyusb/blob/2eaf99e0aa9c10d62dd8d0a4e765f5941bfeaf98/examples/device/audio_4_channel_mic/src/tusb_config.h#L104

--- examples/usb_microphone/usb_microphone.h
+++ examples/usb_microphone/usb_microphone.h
@@ -11,11 +11,11 @@
 #include "tusb.h"
 
 #ifndef SAMPLE_RATE
-#define SAMPLE_RATE (CFG_TUD_AUDIO_EP_SZ_IN / 2) * 1000
+#define SAMPLE_RATE ((CFG_TUD_AUDIO_EP_SZ_IN / 2) - 1) * 1000
 #endif
 
 #ifndef SAMPLE_BUFFER_SIZE
-#define SAMPLE_BUFFER_SIZE (CFG_TUD_AUDIO_EP_SZ_IN/2)
+#define SAMPLE_BUFFER_SIZE ((CFG_TUD_AUDIO_EP_SZ_IN/2) - 1)
 #endif
 
 typedef void (*usb_microphone_tx_ready_handler_t)(void);
